a7b83df17bfde23de73b9557103beb5a794d842b
Otgon <otgon@Otgons-MacBook-Pro.local>
2025-08-29 19:39:26 +0800

    init

---
+++ src/main/java/com/athena/Athena_AI/AthenaAiApplication.java
@@ -0,0 +1,13 @@
+package com.athena.Athena_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athena/Athena_AI/AthenaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athena.Athena_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
